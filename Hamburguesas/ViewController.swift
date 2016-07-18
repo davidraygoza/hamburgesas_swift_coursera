35c37ba992e274e40da7bc0cc5e1f64d8b983ab4
David Raygoza <david_raygoza@MacBook-Pro-de-David-Raygoza-2.local>
2016-07-17 21:38:19 -0500

    first inicial hamburgesas

---
+++ Hamburguesas/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Hamburguesas
+//
+//  Created by David Raygoza
+//  david.raygoza.ramirez@gmail.com
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // @IBoutlet para la etiqueta de país.
+    @IBOutlet weak var pais: UILabel!
+    
+    // @IBoutlet para la etiqueta de hamburguesa.
+    @IBOutlet weak var hamburguesa: UILabel!
+    
+    // @IBoutlet para la etiqueta de precio.
+    @IBOutlet weak var precio: UILabel!
+    
+    // Instancia de la clase ColeccionDePaises.
+    let paises = ColeccionDePaises()
+    
+    // Instancia de la clase ColeccionDeHamburguesas.
+    let hamburguesas = ColeccionDeHamburguesas()
+    
+    // Instancia de la clase ColeccionDePrecios.
+    let precios = ColeccionDePrecios()
+    
+    // Instancia de la estructura Colores.
+    let colores = Colores()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func quieroUnaHamburguesa() {
+        pais.text = paises.obtenPais()
+        hamburguesa.text = hamburguesas.obtenHamburguesa()
+        precio.text = String(precios.obtenPrecio())
+        
+        // Obtengo el valor aleatorio de la estructura.
+        let colorAleatorio = colores.regresaColorAleatorio()
+        
+        // Asigno el color a la vista a través del objeto view
+        view.backgroundColor = colorAleatorio
+        view.tintColor = colorAleatorio
+    }
+}
+
